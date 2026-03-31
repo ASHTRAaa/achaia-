a544684ae82ab9d5302bfcf5813c468f87b66eb5
ASHTRAaa <62059manisg@gmail.com>
2026-03-31 19:19:29 +0530

    navlink in hero and webkit tap added in index

--- src/layout/hero/Hero.jsx
+++ src/layout/hero/Hero.jsx
@@ -1,21 +1,25 @@
 import "./Hero.css"
 import heroimg from "../../assets/heroimg.png";
 import herovideo from "../../assets/herobg.mp4"
+import { NavLink } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="hero">
-      
       <img src={heroimg} alt="" className="hero-image" />
 
-      
-      <video className="hero-video" autoPlay muted loop playsInline preload="none">
+      <video
+        className="hero-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="none"
+      >
         <source src={herovideo} type="video/mp4" />
       </video>
 
-      
       <div className="hero-overlay"></div>
 
-      
       <div className="hero-content">
         <h1>Build Websites, Mobile Apps & AI Products That Scale</h1>
 
@@ -25,9 +29,12 @@ const Hero = () => {
         </p>
 
         <div className="hero-buttons">
-          <button className="btn-primary">View Work</button>
-
-          <button className="btn-secondary">Start a Project</button>
+          <NavLink to="/work">
+            <button className="btn-primary">View Work</button>
+          </NavLink>
+          <NavLink to="/cyp">
+            <button className="btn-secondary">Start a Project</button>
+          </NavLink>
         </div>
       </div>
     </section>
