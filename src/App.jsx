a0eb14f5e7909adfb079fb41ed3dd1ca487ff12b
ASHTRAaa <62059manisg@gmail.com>
2026-04-16 19:23:27 +0530

    added google analytics for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route,useLocation} from "react-router-dom"
+import { useEffect } from "react"
 import MainLayout from "./layout/main-layout/MainLayout.jsx"
 import Home from "./pages/home/Home.jsx"
 import AboutUs from "./pages/about-us/AboutUs.jsx"
@@ -6,6 +7,13 @@ import Contact from "./pages/contact/Contact.jsx"
 import Work from "./pages/work/Work.jsx"
 import Cyp from "./pages/cost-your-project/Cyp.jsx"
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag("config", "G-EP8Y3C2Z1L", {
+      page_path: location.pathname,
+    });
+  }, [location]);
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
